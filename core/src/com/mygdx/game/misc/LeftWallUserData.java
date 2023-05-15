9c05fc3fddbf9fffe26e832848bf5e7e853fc2b0
GGFalco <102793595+GGFalco@users.noreply.github.com>
2023-05-15 22:14:55 +0200

    Delete LeftWallUserData.java

--- core/src/com/mygdx/game/misc/LeftWallUserData.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.game.misc;
-
-public class LeftWallUserData extends UserData{
-
-    public LeftWallUserData(float width, float height) {
-        super(width, height);
-        userDataType = UserDataType.LEFTWALL;
-    }
-}
