d354df7f702345babd2db45373ae28207e8de449
GGFalco <102793595+GGFalco@users.noreply.github.com>
2023-05-15 22:14:11 +0200

    Delete GroundUserData.java

--- core/src/com/mygdx/game/misc/GroundUserData.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.game.misc;
-
-public class GroundUserData extends UserData{
-
-    public GroundUserData(float width, float height) {
-        super(width, height);
-        userDataType = UserDataType.GROUND;
-    }
-}
